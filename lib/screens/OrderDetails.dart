0a6b65646ee91377c8e3bdc501497b8169ab127e
sarik Khan <sarik.sak.khan@gmail.com>
2023-12-07 10:38:38 +0530

    changes in pattern

--- lib/screens/OrderDetails.dart
+++ lib/screens/OrderDetails.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:linked_scroll_controller/linked_scroll_controller.dart';
 // import 'package:horizontal_data_table/scroll/linked_scroll_controller/linked_scroll_controller.dart';
 import 'package:tsf/utils/commonFunctions.dart';
-// import 'package:sync_scroll/sync_scroll.dart';
-// import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+
 
 final List orderDetails = [
   [
@@ -108,8 +107,7 @@ final dispatchDetailsOrder = [
 // and dispatchDetailsOrder lists ...
 // ... Your orderDetails and dispatchDetailsOrder lists ...
 
-// Your orderDetails and dispatchDetailsOrder lists ...
-
+// Your orderDetails and dispatchDetailsOrder lists ..
 class OrderDetails extends StatefulWidget {
   final String? orderId;
   final bool? isOrderPage;
@@ -139,17 +137,15 @@ class _OrderDetailsState extends State<OrderDetails> {
     super.dispose();
   }
 
-  Widget _buildHeader() {
-    // We take the first element of orderDetails for the header titles
-    List<String> headers = orderDetails.map((detail) => detail[0] as String).toList();
-
+  Widget _buildHeader(List<String> headers) {
+    List<String> headerTitles = orderDetails.map((detail) => detail[0].toString()).toList();
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       controller: _headerScrollController,
       child: Row(
         children: headers.map((header) {
           return Container(
-            width: 150, // Adjust width as needed
+            width: 200, // Adjust width as needed
             padding: EdgeInsets.symmetric(vertical: 8),
             child: Text(header, style: TextStyle(fontWeight: FontWeight.bold)),
           );
@@ -158,23 +154,15 @@ class _OrderDetailsState extends State<OrderDetails> {
     );
   }
 
-  Widget _buildRow(Map<String, dynamic> item) {
-    // We take the second element of orderDetails for the row values
-    List<String> fields = orderDetails.map((detail) => detail[1] as String).toList();
-
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      controller: ScrollController(), // New controller for each row for independent horizontal scrolling
-      child: Row(
-        children: fields.map((field) {
-          var value = item[field] ?? 'N/A';
-          return Container(
-            width: 150, // Set a fixed width for each cell
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: Text(value.toString()),
-          );
-        }).toList(),
-      ),
+  Widget _buildRow(Map<String, dynamic> item, List<String> headers) {
+    return Row(
+      children: headers.map((header) {
+        return Container(
+          width: 200, // Set a fixed width for each cell
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: Text(item[header]?.toString() ?? 'N/A'),
+        );
+      }).toList(),
     );
   }
 
@@ -199,15 +187,21 @@ class _OrderDetailsState extends State<OrderDetails> {
               ReturnObj response = snapshot.data!;
               var data = json.decode(json.encode(response.data)); // Assuming 'data' is a JSON array
 
+              List<String> headers = data.isNotEmpty ? data.first.keys.cast<String>().toList() : [];
+
               return Column(
                 children: [
-                  _buildHeader(),
+                  _buildHeader(headers),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        return _buildRow(data[index]);
-                      },
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        controller: _bodyScrollController,
+                        child: Column(
+                          children: data.map<Widget>((item) => _buildRow(item, headers)).toList(),
+                        ),
+                      ),
                     ),
                   ),
                 ],
@@ -224,4 +218,3 @@ class _OrderDetailsState extends State<OrderDetails> {
 }
 
 void main() => runApp(MaterialApp(home: OrderDetails(isOrderPage: true, orderId: '123')));
-
