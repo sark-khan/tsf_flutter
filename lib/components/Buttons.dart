a918f8632a9a1fc7d2db3f546cf56d7c6bf85061
sark-khan <sarikalam@webmobsoft.com>
2024-07-10 15:49:33 +0530

    changes for comment button

--- lib/components/Buttons.dart
+++ lib/components/Buttons.dart
@@ -77,72 +77,83 @@ class Buttons {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
-      child: Container(
-        padding: EdgeInsets.only(top: 12, left: 20, right: 20, bottom: 20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Add Comment', style: TextStyle(fontSize: 18.0)),
-                IconButton(
-                  icon: Icon(Icons.close),
-                  onPressed: () {
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 12,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 20,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Add Comment', style: TextStyle(fontSize: 18.0)),
+                  IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+              TextFormField(
+                controller: commentController,
+                decoration: InputDecoration(
+                  hintText: 'Enter Your Comment here...',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: AppColors().buttonColorPurple, width: 1.0),
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: AppColors().buttonColorPurple, width: 2.0),
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+                // minLines: 3,
+                maxLines: 5,
+                onFieldSubmitted: (value) async {
+                  ReturnObj response = await CommonFunctions()
+                      .addComments(commentController.text, orderId);
+                  Fluttertoast.showToast(msg: response.message);
+                  if (response.status) {
                     Navigator.of(context).pop();
-                  },
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  ReturnObj response = await CommonFunctions()
+                      .addComments(commentController.text, orderId);
+                  Fluttertoast.showToast(msg: response.message);
+                  if (response.status) {
+                    Navigator.of(context).pop();
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      AppColors().buttonColorPurple, // Background color
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 35, vertical: 12),
                 ),
-              ],
-            ),
-            TextFormField(
-              controller: commentController,
-              decoration: InputDecoration(
-                hintText: 'Enter Your Comment here...',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: AppColors().buttonColorPurple, width: 1.0),
-                  borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: AppColors().buttonColorPurple, width: 2.0),
-                  borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                child: Text(
+                  TextConstants().SUBMIT,
+                  style: const TextStyle(color: Colors.white),
                 ),
               ),
-              // minLines: 3,
-              maxLines: 5,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                ReturnObj response = await CommonFunctions()
-                    .addComments(commentController.text, orderId);
-                // if(response.status){
-                Fluttertoast.showToast(msg: response.message);
-                if (response.status) {
-                  Navigator.of(context).pop();
-                }
-                // This will close the dialog box when the 'x' button is pressed
-
-                // }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    AppColors().buttonColorPurple, // Background color
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 35, vertical: 12),
-              ),
-              child: Text(
-                TextConstants().SUBMIT,
-                style: const TextStyle(color: Colors.white),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
